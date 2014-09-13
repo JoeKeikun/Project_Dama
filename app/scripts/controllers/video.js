5816745fc5068ae1779f9ee58e295095f818460a
JoeKeikun <247716557@qq.com>
2014-09-13 23:55:07 +0800

    finish 'video' view;

--- app/scripts/controllers/video.js
+++ app/scripts/controllers/video.js
@@ -8,12 +8,65 @@
  * Controller of the damaApp
  */
 angular.module('damaApp').controller('VideoCtrl', function($scope) {
+    /**---------- 测试数据 ------------**/
+    var testData1 = [{
+        img: 'http://r4.ykimg.com/050C000053F0D7966737B344EB05190B',
+        name: '测试视屏1',
+        time: 1.2,
+        timeunit: '小时',
+        videourl: ''
+    }, {
+        img: 'http://r4.ykimg.com/051500005410FC476737B325230E59D9',
+        name: '测试视屏2',
+        time: 36,
+        timeunit: '分钟',
+        videourl: ''
+    }, {
+        img: 'http://r4.ykimg.com/0515000054138E6B6737B3251F0067D2',
+        name: '测试超长名字测试超长名字测试超长名字测试超长名字测试超长名字测试超长名字测试超长名字测试超长名字测试超长名字测试超长名字',
+        time: 12,
+        timeunit: '分钟',
+        videourl: ''
+    }, {
+        img: 'http://r1.ykimg.com/051500005413B2386737B34DC7085A19',
+        name: '测试视屏',
+        time: 36,
+        timeunit: '分钟',
+        videourl: ''
+    }, {
+        img: 'http://r1.ykimg.com/051500005413B2386737B34DC7085A19',
+        name: '测试视屏',
+        time: 36,
+        timeunit: '分钟',
+        videourl: ''
+    }, {
+        img: 'http://r1.ykimg.com/051500005413B2386737B34DC7085A19',
+        name: '测试视屏',
+        time: 36,
+        timeunit: '分钟',
+        videourl: ''
+    }, {
+        img: 'http://r1.ykimg.com/051500005413B2386737B34DC7085A19',
+        name: '测试视屏',
+        time: 36,
+        timeunit: '分钟',
+        videourl: ''
+    }, {
+        img: 'http://r1.ykimg.com/051500005413B2386737B34DC7085A19',
+        name: '测试视屏',
+        time: 36,
+        timeunit: '分钟',
+        videourl: ''
+    }];
+
     /**---------- 交互数据 ------------**/
     $scope.activetab = {
         video: 'active'
     };
 
     $scope.localNums = 14;
+
+    $scope.videolist = testData1;
 
     //-----------处理函数------------
     // 跳转处理
